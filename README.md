2b3960840f46f56887b27c00cdb4a3117c983b1e
dalia mahmoud <daliamahmoud313@gmail.com>
2022-01-13 16:40:02 +0200

    commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Quiz-App-JS
-Quiz app using HTML-CSS-JS
+## Quiz app using HTML-CSS-JS
+### MennatAllah Abdo
+### Dalia Mohamed
+### Amr Ahmed Hussien
+### Ahmed Mohamed Mohamed Hafez
---
+++ script.js
@@ -0,0 +1,12 @@
+//============ Sections need to change 'background-color' property on male & female choices:
+/*
+body, quizApp, quizArea, answersArea, 
+
+answer, submitButton: hover(mouseover event), results
+
+submitButton 'color' property
+
+value should be var(--maleColor)  or  var(--femaleColor)
+
+=====Example>> try this: answersArea.style.cssText = "background-color: var(--femaleColor)";
+*/
