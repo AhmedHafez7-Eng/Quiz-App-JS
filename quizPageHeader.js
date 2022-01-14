5fdff3e50988414f737be8babec8a8d65aae65d7
AmrAhmedHussien <AmrAhmedHussien@outlook.com>
2022-01-14 22:02:25 +0200

    updates

--- quizPageHeader.js
+++ quizPageHeader.js
@@ -6,7 +6,34 @@ name[0].innerHTML=q.QuizName;
 var mins = q.QuizTime-1;
 var secs = 60;
 var timeUp=false;
-
+document.getElementById("start").addEventListener("click",()=>{
+    setInterval(function timechange(){
+        let stopWatch;
+        if (secs>0) {
+            secs--;
+        }
+        else{
+            secs=60;
+            if (mins>0) {
+                mins--;
+                
+            }
+            else{
+                timeUp=true;
+            }
+        }
+        stopWatch=mins + ":" + secs;
+        if (timeUp) {
+            timeDev[0].innerHTML="Time is up"
+        }
+        else{
+            timeDev[0].innerHTML=stopWatch;
+        }
+        
+        
+    },1000)
+    
+})
 setInterval(function timechange(){
     let stopWatch;
     if (secs>0) {
